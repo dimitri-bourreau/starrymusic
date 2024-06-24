ccecb785b290391d98d7c404fd383844b5feb051
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-24 22:57:57 +0200

    feat: add Music type

---
+++ src/starrysky-music/types/music.type.ts
@@ -0,0 +1,13 @@
+export default interface Music {
+  title: string
+  album: string
+  year: string
+  duration: string
+  credits: {
+    music: string | null
+    lyrics: string | null
+    'lead-vocals': string | null
+    extra: string[] | null
+  } | null
+  lyrics: string[][]
+}
