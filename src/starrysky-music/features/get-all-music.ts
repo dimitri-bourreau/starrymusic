9ae86b61233388d28386d9fefb4b2b2707c28b03
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-24 22:49:10 +0200

    feat: add feature getAllMusic

---
+++ src/starrysky-music/features/get-all-music.ts
@@ -0,0 +1,10 @@
+import espille from '../album/timeshift-fr/setlist/01-espille.music.json'
+import timeshift from '../album/timeshift-fr/setlist/02-timeshift.music.json'
+import interlude1Timeshift from '../album/timeshift-fr/setlist/03-bis-interlude-1.music.json'
+import reverse from '../album/timeshift-fr/setlist/04-re-verse.music.json'
+
+const getallMusic = () => {
+  return [espille, timeshift, interlude1Timeshift, reverse]
+}
+
+export default getallMusic
