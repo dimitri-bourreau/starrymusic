f00b0deb020684793882239b41bb78e557498d40
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-24 21:15:21 +0200

    feat: remove audio provider/components & a white border

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,19 +2,9 @@ import { Fragment } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { AudioProvider } from '@/components/AudioProvider'
-import { AudioPlayer } from '@/components/player/AudioPlayer'
 import { TinyWaveFormIcon } from '@/components/TinyWaveFormIcon'
 import { Waveform } from '@/components/Waveform'
 import posterImage from '@/images/poster.jpg'
-
-function PersonIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 11 12" {...props}>
-      <path d="M5.019 5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Zm3.29 7c1.175 0 2.12-1.046 1.567-2.083A5.5 5.5 0 0 0 5.019 7 5.5 5.5 0 0 0 .162 9.917C-.39 10.954.554 12 1.73 12h6.578Z" />
-    </svg>
-  )
-}
 
 export default function MainLayout({
   children,
@@ -24,7 +14,7 @@ export default function MainLayout({
   let hosts = ['Tai', 'Sasha', 'Yuki', 'Logan', 'Neku']
 
   return (
-    <AudioProvider>
+    <>
       <header className="bg-slate-50 lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120 dark:bg-black">
         <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
           <span className="font-mono text-slate-500 dark:text-zinc-50">
@@ -113,33 +103,11 @@ export default function MainLayout({
           </section>
         </div>
       </header>
-      <main className="border-t border-slate-200 lg:relative lg:mb-28 lg:ml-112 lg:border-t-0 xl:ml-120">
+
+      <main className="border-slate-200 lg:relative lg:mb-28 lg:ml-112 lg:border-t-0 xl:ml-120">
         <Waveform className="absolute left-0 top-0 h-20 w-full" />
         <div className="relative">{children}</div>
       </main>
-      <footer className="border-t border-slate-200 bg-slate-50 py-10 pb-40 sm:py-16 sm:pb-32 lg:hidden">
-        <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4">
-          <h2 className="mt-8 flex items-center font-mono text-sm font-medium leading-7 text-slate-900">
-            <PersonIcon className="h-3 w-auto fill-slate-300" />
-            <span className="ml-2.5">Starrysky</span>
-          </h2>
-          <div className="mt-2 flex gap-6 text-sm font-bold leading-7 text-slate-900">
-            {hosts.map((host, hostIndex) => (
-              <Fragment key={host}>
-                {hostIndex !== 0 && (
-                  <span aria-hidden="true" className="text-slate-400">
-                    /
-                  </span>
-                )}
-                {host}
-              </Fragment>
-            ))}
-          </div>
-        </div>
-      </footer>
-      <div className="fixed inset-x-0 bottom-0 z-10 lg:left-112 xl:left-120">
-        <AudioPlayer />
-      </div>
-    </AudioProvider>
+    </>
   )
 }
