657e21d8bcf73f772df237efc38c9f82fc1cc86e
Dimitri Bourreau <contact@dimitribourreau.me>
2024-06-24 21:08:29 +0200

    feat: remove about section

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,13 +2,11 @@ import { Fragment } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { AboutSection } from '@/components/AboutSection'
 import { AudioProvider } from '@/components/AudioProvider'
 import { AudioPlayer } from '@/components/player/AudioPlayer'
 import { TinyWaveFormIcon } from '@/components/TinyWaveFormIcon'
 import { Waveform } from '@/components/Waveform'
 import posterImage from '@/images/poster.jpg'
-
 
 function PersonIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -27,9 +25,11 @@ export default function MainLayout({
 
   return (
     <AudioProvider>
-      <header className="bg-slate-50 dark:bg-black lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120">
+      <header className="bg-slate-50 lg:fixed lg:inset-y-0 lg:left-0 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120 dark:bg-black">
         <div className="hidden lg:sticky lg:top-0 lg:flex lg:w-16 lg:flex-none lg:items-center lg:whitespace-nowrap lg:py-12 lg:text-sm lg:leading-7 lg:[writing-mode:vertical-rl]">
-          <span className="font-mono text-slate-500 dark:text-zinc-50">Starrysky</span>
+          <span className="font-mono text-slate-500 dark:text-zinc-50">
+            Starrysky
+          </span>
           <span className="mt-6 flex gap-6 font-bold text-slate-900 dark:text-zinc-500">
             {hosts.map((host, hostIndex) => (
               <Fragment key={host}>
@@ -43,10 +43,10 @@ export default function MainLayout({
             ))}
           </span>
         </div>
-        <div className="relative z-10 mx-auto px-4 pb-4 pt-10 sm:px-6 md:max-w-2xl md:px-4 lg:min-h-full lg:flex-auto lg:border-x lg:border-slate-200 dark:border-slate-800 lg:px-8 lg:py-12 xl:px-12">
+        <div className="relative z-10 mx-auto px-4 pb-4 pt-10 sm:px-6 md:max-w-2xl md:px-4 lg:min-h-full lg:flex-auto lg:border-x lg:border-slate-200 lg:px-8 lg:py-12 xl:px-12 dark:border-slate-800">
           <Link
             href="/"
-            className="relative mx-auto block w-48 overflow-hidden rounded-lg bg-slate-200 shadow-xl shadow-slate-200 dark:shadow-slate-800 sm:w-64 sm:rounded-xl lg:w-auto lg:rounded-2xl"
+            className="relative mx-auto block w-48 overflow-hidden rounded-lg bg-slate-200 shadow-xl shadow-slate-200 sm:w-64 sm:rounded-xl lg:w-auto lg:rounded-2xl dark:shadow-slate-800"
             aria-label="Homepage"
           >
             <Image
@@ -63,12 +63,12 @@ export default function MainLayout({
               <Link href="/">starrymusic.fr</Link>
             </p>
             <p className="mt-3 text-lg font-medium leading-8 text-slate-700 dark:text-zinc-300">
-              Site réalisé par des fans de Starrysky pour regrouper leurs musiques avec leurs paroles.
+              Site réalisé par des fans de Starrysky pour regrouper leurs
+              musiques avec leurs paroles.
             </p>
           </div>
-          <AboutSection className="mt-12 hidden lg:block" />
           <section className="mt-10 lg:mt-12">
-            <h2 className="sr-only flex items-center font-mono text-sm font-medium leading-7 text-slate-900 dark:text-slate-400 lg:not-sr-only">
+            <h2 className="sr-only flex items-center font-mono text-sm font-medium leading-7 text-slate-900 lg:not-sr-only dark:text-slate-400">
               <TinyWaveFormIcon
                 colors={['fill-indigo-300', 'fill-blue-300']}
                 className="h-2.5 w-2.5"
@@ -83,9 +83,18 @@ export default function MainLayout({
               {(
                 [
                   ['Site officiel de Starrysky', 'https://www.starrysky.fr/'],
-                  ['Instagram de Starrysky', 'https://www.instagram.com/starrysky_band/'],
-                  ['Playlists pour découvrir Starrysky', 'https://linktr.ee/discover_starrysky'],
-                  ['Code source', 'https://github.com/dimitri-bourreau/starrymusic'],
+                  [
+                    'Instagram de Starrysky',
+                    'https://www.instagram.com/starrysky_band/',
+                  ],
+                  [
+                    'Playlists pour découvrir Starrysky',
+                    'https://linktr.ee/discover_starrysky',
+                  ],
+                  [
+                    'Code source',
+                    'https://github.com/dimitri-bourreau/starrymusic',
+                  ],
                 ] as const
               ).map(([label, link]) => (
                 <li key={label} className="flex">
@@ -94,7 +103,9 @@ export default function MainLayout({
                     className="group flex items-center"
                     aria-label={label}
                   >
-                    <span className="hidden sm:block dark:text-zinc-300">{label}</span>
+                    <span className="hidden sm:block dark:text-zinc-300">
+                      {label}
+                    </span>
                   </Link>
                 </li>
               ))}
@@ -108,7 +119,6 @@ export default function MainLayout({
       </main>
       <footer className="border-t border-slate-200 bg-slate-50 py-10 pb-40 sm:py-16 sm:pb-32 lg:hidden">
         <div className="mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4">
-          <AboutSection />
           <h2 className="mt-8 flex items-center font-mono text-sm font-medium leading-7 text-slate-900">
             <PersonIcon className="h-3 w-auto fill-slate-300" />
             <span className="ml-2.5">Starrysky</span>
