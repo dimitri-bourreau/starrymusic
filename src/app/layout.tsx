f85c246c82a879f54aa9a087f1a6770a66232ba8
Dimitri Bourreau <contact@dimitribourreau.me>
2024-07-06 14:58:52 +0200

    feat: add favicon and app icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,7 @@ import '@/styles/tailwind.css'
 export const metadata: Metadata = {
   title: {
     template: '%s - starrymusic.fr',
-    default:
-      'starrymusic - Musiques et paroles de Starrysky',
+    default: 'starrymusic - Musiques et paroles de Starrysky',
   },
   description:
     'Site réalisé par des fans de Starrysky pour regrouper leurs musiques avec leurs paroles.',
@@ -18,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full bg-white dark:bg-black antialiased">
+    <html lang="fr" className="h-full bg-white antialiased dark:bg-black">
       <head>
         <link
           rel="preconnect"
@@ -29,6 +28,27 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://api.fontshare.com/v2/css?f[]=satoshi@700,500,400&display=swap"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="flex min-h-full">
         <div className="w-full">{children}</div>
