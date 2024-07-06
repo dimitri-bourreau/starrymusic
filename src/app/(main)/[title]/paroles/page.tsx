ae473146737783a2607a2e56c4ea8d0e690c7eed
Dimitri Bourreau <contact@dimitribourreau.me>
2024-07-06 10:59:35 +0200

    feat: if no lyrics, display explanation

--- src/app/(main)/[title]/paroles/page.tsx
+++ src/app/(main)/[title]/paroles/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
+import getMusic from '@/starrysky-music/features/get-music'
+
+interface PageProps {
+  params: { title: string }
+}
+
+export default function Page({ params }: PageProps) {
+  const music = getMusic(decodeURIComponent(params.title))
+
+  if (!music)
+    return (
+      <p className="ml-4 mt-10">
+        Impossible de trouver les paroles, contactez Dimitri, c&apos;est louche
+        ! 🤔
+      </p>
+    )
+
+  if (music.lyrics === null)
+    return (
+      <p className="ml-4 mt-10 text-gray-500">
+        Cette musique ne contient pas de paroles.
+      </p>
+    )
+
   return <p>OK</p>
 }
