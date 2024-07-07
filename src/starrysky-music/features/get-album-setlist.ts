f3dea4eb4e6ab4baf09973319e2cc30d77796dc7
Dimitri Bourreau <contact@dimitribourreau.me>
2024-07-07 14:57:51 +0200

    feat: add NEW MAGICAL GIRL

--- src/starrysky-music/features/get-album-setlist.ts
+++ src/starrysky-music/features/get-album-setlist.ts
@@ -3,12 +3,13 @@ import espille from '@/starrysky-music/album/timeshift-fr/setlist/01-espille.mus
 import timeshift from '@/starrysky-music/album/timeshift-fr/setlist/02-timeshift.music.json'
 import interlude1Timeshift from '@/starrysky-music/album/timeshift-fr/setlist/03-bis-interlude-1.music.json'
 import reverse from '@/starrysky-music/album/timeshift-fr/setlist/04-re-verse.music.json'
+import newMagicalGirl from '../album/timeshift-fr/setlist/05-new-magical-girl.json'
 import Music from '@/starrysky-music/types/music.type'
 
 const getAlbumSetlist = (title: Album['title']): Music[] => {
   switch (title) {
     case "TIMESHIFT - L'album qui s'écoute à l'envers":
-      return [espille, timeshift, interlude1Timeshift, reverse]
+      return [espille, timeshift, interlude1Timeshift, reverse, newMagicalGirl]
     default:
       console.error(`Failed to get setlist for album named ${title}`)
       return []
