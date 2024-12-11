4eaddd63d8161ee4248534ee23b1899849503c51
Dimitri Bourreau <contact@dimitribourreau.me>
2024-12-11 12:52:39 +0100

    feat: update album's card for wiki fandom, new text

--- src/app/(main)/albums/[title]/page.tsx
+++ src/app/(main)/albums/[title]/page.tsx
@@ -99,14 +99,10 @@ export default async function Page({ params }: PageProps) {
                 <div className="bg-white  shadow sm:rounded-lg dark:bg-slate-900">
                   <div className="px-4 py-5 sm:p-6">
                     <h3 className="text-base font-semibold text-gray-900 dark:text-gray-300">
-                      Page Wiki fandom
+                      En savoir plus
                     </h3>
                     <div className="mt-2 max-w-xl text-sm text-gray-400">
-                      <p>
-                        Découvrez un max de détails de{' '}
-                        <span className="font-bold">{album.title}</span> sur le
-                        wiki fandom !
-                      </p>
+                      <p>On a un wiki ultra complet ! ✌️</p>
                     </div>
                     <div className="mt-5">
                       <Link
@@ -118,7 +114,7 @@ export default async function Page({ params }: PageProps) {
                           type="button"
                           className="inline-flex items-center rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-500"
                         >
-                          Plus sur starrysky.fandom.com
+                          Page wiki de {album.title}
                         </button>
                       </Link>
                     </div>
