6d875a2d1829c1f802e927706248154c31a5b030
Dimitri Bourreau <contact@dimitribourreau.me>
2025-01-21 15:07:28 +0100

    feat: add login page

---
+++ src/app/(main)/connexion/page.tsx
@@ -0,0 +1,71 @@
+export default async function Login() {
+  return (
+    <div className="flex min-h-full flex-1">
+      <div className="flex flex-1 flex-col justify-start px-4 sm:px-6 lg:flex-none">
+        <div className="mx-auto w-full max-w-sm lg:w-96">
+          <div>
+            <h2 className="text-2xl/9 font-bold tracking-tight text-gray-900">
+              Se connecter
+            </h2>
+            <p className="mt-2 text-sm/6 text-gray-500">
+              Pour l&apos;instant réservé aux invités
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <div>
+              <form action="#" method="POST" className="space-y-6">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      required
+                      autoComplete="email"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Mot de passe
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      required
+                      autoComplete="current-password"
+                      className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Se connecter
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/client/SideNavigation.tsx
+++ src/components/client/SideNavigation.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 import ExternalLinks from '@/components/server/ExternalLinks'
 import PictureTitleAndSubtitle from '../server/PictureTitleAndSubtitle'
 import InternalLinks from '../server/InternalLinks'
+import Link from 'next/link'
 
 export default function SideNavigation() {
   return (
@@ -16,8 +17,35 @@ export default function SideNavigation() {
       )}
     >
       <PictureTitleAndSubtitle />
+
       <InternalLinks />
-      <ExternalLinks />
+
+      <div className="flex items-end justify-between">
+        <ExternalLinks />
+
+        <Link href="/connexion">
+          <button
+            type="button"
+            className="h-10 w-10 rounded-full p-1.5 text-white transition-all hover:h-12 hover:w-12 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            <svg
+              fill="none"
+              stroke-width="1.5"
+              stroke="black"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              data-slot="icon"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              ></path>
+            </svg>
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
